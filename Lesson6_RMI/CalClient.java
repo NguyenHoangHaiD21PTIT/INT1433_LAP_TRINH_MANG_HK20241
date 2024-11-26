3e9c372600da09b33bc21da1dacd97e3a0eb944d
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-27 03:23:33 +0700

    Delete Lesson6_RMI directory

--- Lesson6_RMI/CalClient.java
+++
@@ -1,8 +0,0 @@
-package Lesson6_RMI;
-import java.rmi.*;
-public class CalClient {
-    public static void main(String[] args) throws Exception{
-        CalInf cal = (CalInf)Naming.lookup("rmi://localhost/call03");
-        System.out.println("Sum: " + cal.add(100,200));
-    }
-}
--- Lesson6_RMI/CalInf.java
+++
@@ -1,5 +0,0 @@
-package Lesson6_RMI;
-import java.rmi.*;
-public interface CalInf extends Remote{
-    public int add(int a, int b) throws RemoteException;
-}
