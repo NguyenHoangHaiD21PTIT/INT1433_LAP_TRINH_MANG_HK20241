4c2fc6cf1023d79ae6d0b263d0e578a8c7ad8a91
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-12-13 16:41:13 +0700

    Update PTichTSNT.java

--- WebService/B21DCCN001/PTichTSNT.java
+++ WebService/B21DCCN001/PTichTSNT.java
@@ -8,20 +8,22 @@ public class PTichTSNT {
         DataService port = service.getDataServicePort();
         List<Integer> a = port.getData(msv, qCode);
         System.out.println(a);
-        List<vn.medianews.ArrayList> res = new java.util.ArrayList<>();
-        for (int x : a) {
-            vn.medianews.ArrayList tmp = new vn.medianews.ArrayList();
-            for (int i = 2; i * i <= x; i++) {
-                while (x % i == 0) {
-                    x /= i;
-                    tmp.getItem().add(i);  
+        List<String>ans = new java.util.ArrayList<>();
+        for(int x: a){
+            String tmp = "";
+            for(int i = 2;i*i<=x;i++){
+                while(x%i==0){
+                    x/=i;
+                    tmp+=String.format("%d, ", i);
                 }
             }
-            if (x > 1) tmp.getItem().add(x);  
-            res.add(tmp);
+            if(x > 1) tmp+=String.format("%d, ", x);
+            tmp = tmp.trim();
+            tmp = tmp.substring(0, tmp.length() - 1);
+            ans.add(tmp);
+            System.out.println(tmp);
         }
-        System.out.println(res.get(0).getItem());
-        port.submitDataIntMatrix(msv, qCode, res);
+        port.submitDataStringArray(msv, qCode, ans);
     }
 }
-//Chờ
+
