1fa8cece0103605883810ddd5d90135ec4ea335d
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-12-13 16:41:31 +0700

    Update QuanLySinhVien.java

--- WebService/B21DCCN001/QuanLySinhVien.java
+++ WebService/B21DCCN001/QuanLySinhVien.java
@@ -1,41 +1,26 @@
 package WebService.B21DCCN001;
-
 import vn.medianews.*;
 import java.util.*;
-
 public class QuanLySinhVien {
     public static void main(String[] args) throws Exception {
         String msv = "B21DCCN001", qCode = "2RjVBzv0"; // Mã câu hỏi
         ObjectService_Service service = new ObjectService_Service();
         ObjectService port = service.getObjectServicePort();
-
-        // Lấy danh sách sinh viên từ server
         List<StudentY> a = (List<StudentY>) port.requestListStudentY(msv, qCode);
-        System.out.println("Danh sách nhận được từ server:");
         for (StudentY x : a) System.out.println(x);
         System.out.println();
-
-        // Lọc sinh viên đạt điểm cao nhất theo từng môn học
-        Map<String, StudentY> hsg = new HashMap<>();
-        for (StudentY x : a) {
+        Map<String, Float> hsg = new HashMap<>(); //key: tên môn, value: điểm thi cao nhất môn đó
+        for(StudentY x: a){
             String mon = x.getSubject();
-            if (!hsg.containsKey(mon) || x.getScore() > hsg.get(mon).getScore()) {
-                hsg.put(mon, x);
-            }
+            Float diemMon = x.getScore();
+            if ((!hsg.containsKey(mon)) || (diemMon > hsg.get(mon))) hsg.put(mon, diemMon);
         }
-
-        // Chuyển danh sách từ Map sang List
-        List<StudentY> ans = new java.util.ArrayList<>(hsg.values());
-
-        // Sắp xếp danh sách theo thứ tự tăng dần của subject
-        ans.sort(Comparator.comparing(StudentY::getSubject));
-
-        // In danh sách trước khi gửi
-        System.out.println("Danh sách gửi lên server:");
-        for (StudentY x : ans) System.out.println(x);
-
-        // Gửi danh sách đã sắp xếp lên server
+        List<Float>diemMax = new java.util.ArrayList<>(hsg.values());
+        List<StudentY>ans = new java.util.ArrayList<>();
+        for(StudentY x: a){
+            if(diemMax.contains(x.getScore())) ans.add(x);
+        }
         port.submitListStudentY(msv, qCode, ans);
     }
 } 
-//Đang bug
+
