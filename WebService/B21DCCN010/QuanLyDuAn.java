926032ae27fa073a968023328b6a372a3397c4c4
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-12-17 21:40:51 +0700

    Update QuanLyDuAn.java

--- WebService/B21DCCN010/QuanLyDuAn.java
+++ WebService/B21DCCN010/QuanLyDuAn.java
@@ -1,5 +1,6 @@
 package WebService.B21DCCN010;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import vn.medianews.*;
 import java.util.*;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -11,16 +12,19 @@ public class QuanLyDuAn {
         ObjectService port = service.getObjectServicePort();
         List<Project>a = (List<Project>)port.requestListProject(msv, qCode);
         for(Project p: a) System.out.println(p);
-        List<Project> done = new java.util.ArrayList<>();
-        //Không AC được
+        List<Project> send = new java.util.ArrayList<>();
+        for(Project x : a){
+            LocalDate end = x.getDueDate().toGregorianCalendar().toZonedDateTime().toLocalDate();
+            LocalDate start = LocalDate.now();
+            long days = ChronoUnit.DAYS.between(start, end);
+            if(x.getCompletionPercentage() >= 80 && days <= 15 && days > 0){
+                send.add(x);
+                System.out.println(x + " " + days);
+            }
+        }
+        port.submitListProject(msv, qCode, send);
     }
 }
-
-
-
-
-
-
 
 
 
