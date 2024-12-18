1ddedfbc5c9dea3537166ff63a4ac6e522fa7e2e
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-12-18 19:20:35 +0700

    Update QuanLyNhanVien2.java

--- WebService/B21DCCN084/QuanLyNhanVien2.java
+++ WebService/B21DCCN084/QuanLyNhanVien2.java
@@ -1,7 +1,7 @@
 package WebService.B21DCCN084;
 import vn.medianews.*;
-import java.util.*;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 public class QuanLyNhanVien2 {
     public static void main(String[] args) throws Exception{
         String msv = "B21DCCN084", qCode = "L8CainEX"; 
@@ -9,17 +9,17 @@ public class QuanLyNhanVien2 {
         ObjectService port = service.getObjectServicePort();
         Employee e = port.requestEmployee(msv, qCode);
         System.out.println(e);
-        Calendar start = Calendar.getInstance(), end = Calendar.getInstance();
-        start.setTime(e.getStartDate().toGregorianCalendar().getTime());
-        end.setTime(e.getEndDate().toGregorianCalendar().getTime());
-        int workingDays = 0;
-        while (!start.after(end)) {
-            int dayOfWeek = start.get(Calendar.DAY_OF_WEEK);
-            if (dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY) workingDays++;
-            start.add(Calendar.DAY_OF_MONTH, 1);
+        String bd = e.getStartDate().toString(), kt = e.getEndDate().toString();
+        bd = bd.substring(0, 10); kt = kt.substring(0, 10);
+        LocalDate stDay = LocalDate.parse(bd), enDay = LocalDate.parse(kt);
+        long tmp = ChronoUnit.DAYS.between(stDay, enDay);
+        int cnt = 0;
+        for(int i = 0;i<=tmp;i++){
+            LocalDate curDay = stDay.plusDays(i);
+            if(curDay.getDayOfWeek().getValue()<=5) cnt++;
         }
-        e.setWorkingDays(workingDays);
-        port.submitEmployee(msv, qCode, e);
+        e.setWorkingDays(cnt);
         System.out.println(e);
+        port.submitEmployee(msv, qCode, e);
     }
 }
