ea2c970531961357a920fdee9158aa32a139d7af
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-09-11 21:58:59 +0700

    Test bài này đã được fix

--- ThiTCP_2310/B21DCCN169_HE/DiaChi.java
+++ ThiTCP_2310/B21DCCN169_HE/DiaChi.java
@@ -1,19 +1,21 @@
-package B21DCCN169_HE;
 import java.io.*;
 import java.net.*;
 import TCP.Address;
 public class DiaChi {
+    public static String chuanHoa0(String s){//Viết hoa chữ cái đầu, còn lại thường
+        return Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
+    }
+    public static String chuanHoa1 (String s){ //Chuẩn hoá địa chỉ
+        String ans = "";
+        String []tmp = s.trim().split("\\s+");
+        for(String x: tmp) ans+=chuanHoa0(loaiBo1(x)) + " ";
+        return ans;
+    }
     public static String loaiBo1 (String s){ //Loại bỏ ký tự đặc biệt
         String ans = "";
         for(char x: s.toCharArray()){
             if(Character.isAlphabetic(x) || Character.isDigit(x)) ans+=String.format("%c", x);
         }
-        return ans;
-    }
-    public static String chuanHoa1 (String s){ //Chuẩn hoá địa chỉ
-        String ans = "";
-        String []tmp = s.trim().split("\\s+");
-        for(String x: tmp) ans+=loaiBo1(x) + " ";
         return ans;
     }
     public static String loaiBo2 (String s){ //Loại bỏ ký tự đặc biệt
@@ -27,7 +29,7 @@ public class DiaChi {
         Socket socket = new Socket("203.162.10.109", 2209);
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        String code = "B21DCCN377;4Hk2Ww16";
+        String code = "B22DCCN786;AjecapMw";
         out.writeObject(code);
         out.flush();
         Address ad = (Address)in.readObject();
@@ -40,22 +42,3 @@ public class DiaChi {
         in.close();out.close();socket.close();
     }
 }
-/* run:
-Exception in thread "main" java.io.WriteAbortedException: writing aborted; java.io.NotSerializableException: TCP.Address
-	at java.base/java.io.ObjectInputStream.readObject0(ObjectInputStream.java:1660)
-	at java.base/java.io.ObjectInputStream.readObject(ObjectInputStream.java:495)
-	at java.base/java.io.ObjectInputStream.readObject(ObjectInputStream.java:453)
-	at TCP.DiaChi.main(DiaChi.java:32)
-Caused by: java.io.NotSerializableException: TCP.Address
-	at java.io.ObjectOutputStream.writeObject0(Unknown Source)
-	at java.io.ObjectOutputStream.writeObject(Unknown Source)
-	at Utils.QuestionUtils.Q1449(QuestionUtils.java:3746)
-	at TCP.ObjectStreamHandler.run(ObjectStreamHandler.java:65)
-	at java.util.concurrent.Executors$RunnableAdapter.call(Unknown Source)
-	at java.util.concurrent.FutureTask.run(Unknown Source)
-	at java.util.concurrent.ThreadPoolExecutor.runWorker(Unknown Source)
-	at java.util.concurrent.ThreadPoolExecutor$Worker.run(Unknown Source)
-	at java.lang.Thread.run(Unknown Source)
-C:\Users\Hai\AppData\Local\NetBeans\Cache\25\executor-snippets\run.xml:111: The following error occurred while executing this line:
-C:\Users\Hai\AppData\Local\NetBeans\Cache\25\executor-snippets\run.xml:68: Java returned: 1
-BUILD FAILED (total time: 0 seconds) */
